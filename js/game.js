3445ae004344a77becce8a3e632763922dea282c
mai <61070214@kmitl.ac.th>
2019-02-10 14:08:11 +0700

    Enemy Random Character

--- js/game.js
+++ js/game.js
@@ -10,6 +10,11 @@ const GAME_HEIGHT = 600;
 const PLAYER_WIDTH = 20;
 const PLAYER_HEIGHT = 90;
 const PLAYER_HEALTH = 10;
+const PLAYER_MINX = PLAYER_WIDTH;
+const PLAYER_MAXX = GAME_WIDTH - PLAYER_WIDTH - 200;
+const PLAYER_MINY = PLAYER_HEIGHT - 60;
+const PLAYER_MAXY = GAME_HEIGHT - PLAYER_HEIGHT;
+
 const PLAYER_MAX_SPEED = 600.0;
 const LASER_MAX_SPEED = 300.0;
 const LASER_COOLDOWN = 0.5;
@@ -22,7 +27,15 @@ const Boss_Enemy_HORIZONTAL_PADDING = 80;
 const Boss_Enemy_VERTICAL_PADDING = 70;
 const Boss_Enemy_VERTICAL_SPACING = 80;
 const Boss_Enemy_COOLDOWN = 1.0;
+const Boss_Enemy_SPEED = 2;
+const Boss_Enemy_MOVEDELAY = 500;
+const Boss_Enemy_MINX = 0;
+const Boss_Enemy_MAXX = GAME_WIDTH - 60;
+const Boss_Enemy_MINY = 70;
+const Boss_Enemy_MAXY = GAME_HEIGHT - 80;
+const Boss_Enemy_STYLE = 11;
 var ON_PAUSE = false;
+
 
 const GAME_STATE = {
     Boss_Enemyhealth: 1,
@@ -108,16 +121,8 @@ function updatePlayer(dt, $container) {
     GAME_STATE.playerY += dt * PLAYER_MAX_SPEED;
   }
 
-  GAME_STATE.playerX = clamp(
-    GAME_STATE.playerX,
-    PLAYER_WIDTH,
-    GAME_WIDTH - PLAYER_WIDTH - 200
-  );
-  GAME_STATE.playerY = clamp(
-    GAME_STATE.playerY,
-    PLAYER_HEIGHT - 60,
-    GAME_HEIGHT - PLAYER_HEIGHT
-  );
+  GAME_STATE.playerX = clamp(GAME_STATE.playerX, PLAYER_MINX, PLAYER_MAXX,);
+  GAME_STATE.playerY = clamp(GAME_STATE.playerY, PLAYER_MINY, PLAYER_MAXY,);
 
   if (GAME_STATE.spacePressed && GAME_STATE.playerCooldown <= 0) {
     createLaser($container, GAME_STATE.playerX, GAME_STATE.playerY);
@@ -141,7 +146,7 @@ function createLaser($container, x, y) {
   const audio = new Audio("sound/sfx-laser1.ogg");
   audio.play();
   setPosition($element, x, y);
-  GAME_STATE.Boss_Enemyup = Math.random() < 0.5 ? true : false;
+  enemymove();
 }
 function boom($container, x, y) {
   const boom = document.createElement("img");
@@ -201,7 +206,8 @@ function createBoss_Enemy($container) {
     const y = START_BossY;
     document.querySelector("#E_health").style.width = `100%`;
   const $element = document.createElement("img");
-  $element.src = "img/Enemy-blue-1.png";
+  $element.src = `img/enemy/${parseInt(Math.random() * Boss_Enemy_STYLE) + 1}.gif`;
+  $element.style.width = `${parseInt(Math.random() * 3) * 40 + 40}px`;
   $element.className = "Boss_Enemy";
   $container.appendChild($element);
   const Boss_Enemy = {
@@ -215,22 +221,28 @@ function createBoss_Enemy($container) {
   setPosition($element, x, y);
 }
 
-setInterval(function(){
-    GAME_STATE.Boss_Enemyup = Math.random() < 0.5 ? true : false;
-}, 500)
+
+function enemymove(){
+  GAME_STATE.Boss_Enemyup = Math.random() < 0.5 ? true : false;
+}
+
 function updateEnemies(dt, $container) {
-//   const dx = Math.sin(GAME_STATE.lastTime / 1000.0) * 50;
-//   const dy = Math.cos(GAME_STATE.lastTime / 1000.0) * 10;
         GAME_STATE.Boss_EnemyX = Math.sin(GAME_STATE.lastTime / 1000.0) * 50;
-        GAME_STATE.Boss_EnemyY += (GAME_STATE.Boss_Enemyup == true? 1: -1) * 1;
+        GAME_STATE.Boss_EnemyY += (GAME_STATE.Boss_Enemyup == true? 1: -1) * Boss_Enemy_SPEED;
 
   const enemies = GAME_STATE.enemies;
   for (let i = 0; i < enemies.length; i++) {
     const Boss_Enemy = enemies[i];
     var x = Boss_Enemy.x + GAME_STATE.Boss_EnemyX;
     var y = Boss_Enemy.y + GAME_STATE.Boss_EnemyY;
-    x = clamp(x, 0, GAME_WIDTH-40);
-    y = clamp(y, 70, GAME_HEIGHT-65);
+    x = clamp(x, Boss_Enemy_MINX, Boss_Enemy_MAXX);
+    y = clamp(y, Boss_Enemy_MINY, Boss_Enemy_MAXY);
+    if (y <= Boss_Enemy_MINY){
+      GAME_STATE.Boss_Enemyup = true;
+    }
+    else if(y >= Boss_Enemy_MAXY){
+      GAME_STATE.Boss_Enemyup = false;
+    }
     setPosition(Boss_Enemy.$element, x, y);
     Boss_Enemy.cooldown -= dt;
     if (Boss_Enemy.cooldown <= 0) {
@@ -295,6 +307,7 @@ function destroyBoss_Enemylaser($container, laser) {
 
 function init() {
   const $container = document.querySelector(".game");
+  setInterval(enemymove, Boss_Enemy_MOVEDELAY);
   createPlayer($container);
   createBoss_Enemy($container);
 //   const Boss_EnemySpacing =(GAME_WIDTH - Boss_Enemy_HORIZONTAL_PADDING * 2) / (ENEMIES_PER_ROW - 1);
